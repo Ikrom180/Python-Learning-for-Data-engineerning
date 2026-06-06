022d93ec01078b3534687acec32bf30f3b825633
ikrom724 <ikrompersonal@gmail.com>
2026-06-06 14:51:21 +0500

    Conditions and Tracking

---
+++ if statements.py
@@ -0,0 +1,61 @@
+#if = Do some code only IF some condition is Trus
+#     Else do something else
+
+# age = int(input("Enter your age: "))
+
+# if age > 100:
+#     print("You are too old to vote")
+# elif age >= 18:
+#     print('You are old enough to vote.')
+# elif age < 0:
+#     print('You are not old enough to vote.')
+# else :
+#     print('You must be 18+  to vote.')
+
+
+# response = input("Would you like food? (Y/N)")
+
+# if response == 'Y' or response == 'y':
+#     print('You can have some food')
+# else:
+#     print('You cannot have some food')
+
+
+
+
+# name = input('Enter your name: ')
+
+# if name == '':
+#     print('You did not entered your name !')
+# else:
+#     print(f'Hello {name}!')
+
+
+# for_sale = True
+
+# if for_sale:
+#     print('You can sale this one')
+# else:
+#     print('You can not sale')
+
+
+# online = True
+
+# if online:
+#     print('This user is online')
+# else:
+#     print('This user is not online')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
