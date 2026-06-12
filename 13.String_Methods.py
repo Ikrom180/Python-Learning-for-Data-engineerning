7247407c9123b8580b5cb3f2364f04f78df050f8
ikrom724 <ikrompersonal@gmail.com>
2026-06-12 18:05:32 +0500

    String Methods

---
+++ 13.String_Methods.py
@@ -0,0 +1,17 @@
+name = input("Enter your name: ")
+# result = len(name) =>  len method shows length string
+# result = name.find(" ") => find from indexation index begin from 0 : Bro Code => show result -> 3
+# result = name.rfind("o") #=> find values reverse order
+# name = name.capitalize() #=>  it will return string and make first letter with upper case
+# name = name.upper()
+# name = name.lower()
+# name = name.title() #=> like capitalize
+# name = name.isdigit() # this method find the number only is number comes with string it will return false -> this method return bool
+# result = name.count('ikrom') #=> it will count what ever me need
+replaced = name.replace('i','a') #=> replace method return str
+# print(help(int)) => 'help' it will help to identify which attribute to do what
+
+
+# print(name)
+# print(result)
+print(replaced)
