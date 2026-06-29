dae8327872f928eb0935cfc7d42f51b4d19b0875
ikrom724 <ikrompersonal@gmail.com>
2026-06-29 17:53:13 +0500

    strint_indexxx

---
+++ string_indexing.py
@@ -0,0 +1,9 @@
+# indexing = accessing elements of a sequence using [] (indexing operator)
+#            [start: end : step]
+from time import process_time_ns
+
+credit_number = '1234-5678-9012-3456'
+
+# print(credit_number[5]) # => it count from 0
+# print(credit_number[0:4]) # => [start : end]
+
