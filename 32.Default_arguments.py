83bcd59b6c2578817eaf0ddfa90113fff60672bb
Ikrom180 <ikrom6617@icloud.com>
2026-07-10 01:35:05 +0500

    *Args

--- 32.Default_arguments.py
+++ 32.Default_arguments.py
@@ -1,3 +1,4 @@
+import time
 #default args = A default value for certain parameters
 #               default is used when that argument is ommited
 #               make your functions more flexiable, reduces # of arguments
@@ -6,7 +7,18 @@
 
 
 
-def net_price(list_price, dicount = 0, tax = 0.5):
-    return list_price * (1 - dicount) * (0 + tax)
+# def net_price(list_price, dicount = 0, tax = 0.5):
+#     return list_price * (1 - dicount) * (1 + tax)
+#
+# print(net_price(500))
+# print(net_price(1000, 0.1))
+# print(net_price(1000, 0.1, 0 ))
 
-print(net_price(500))
+
+def count(end, start=1):
+    for x in range(start, end+1):
+        print(x)
+        time.sleep(1)
+    print("Done!")
+
+count(30, 15)
