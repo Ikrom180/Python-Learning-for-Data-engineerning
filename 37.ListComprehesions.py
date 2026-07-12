1728497dc32c9862f7ef8be6ea0b56fce5dac883
Ikrom Windows <ikrompersonal@gmail.com>
2026-07-12 23:42:39 +0500

    List Comp

--- 37.ListComprehesions.py
+++ 37.ListComprehesions.py
@@ -1,1 +1,14 @@
-#list comprehession = A concise
+#list comprehession = A concise way to create lists in Python
+#                     Compact and easier to read than traditional loops
+#                     [expression for value in iterable if condition]
+
+# doubles = []
+
+# for x in range (1,11):
+#     doubles.append(x * 2)
+#
+# print(doubles, end=' ')
+
+# we make it easyer
+doubles = [x * 2 for x in range(1,11)]
+print(doubles)
