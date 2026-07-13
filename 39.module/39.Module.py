334de2a712943b231d0d78fbd30ec535951bd006
ikrom724 <ikrompersonal@gmail.com>
2026-07-13 17:36:32 +0500

    If name started

---
+++ 39.module/39.Module.py
@@ -0,0 +1,34 @@
+# module = a file containing code you want to include in your program
+#          use 'import' to include a module  (built-in or your own)
+#          useful to break up a large program reusable separate files
+import math
+# import math
+#
+# print(math.pi)
+
+# import math as m   -> alias
+#
+# print(m.pi)
+
+
+# from math import e #-> #  moduling like this option is not safe
+
+# a, b, c, d, e = 1, 2, 3, 4, 5
+#
+# # math.e -> is safe option to use
+#
+# print(e ** a)
+# print(e ** b)
+# print(e ** c)
+# print(e ** d)
+
+# import creatingModule
+#
+# result = creatingModule.area(3)
+# result = creatingModule.pi
+# result = creatingModule.cube(3)
+# result = creatingModule.square(3)
+# result = creatingModule.circumference(3)
+#
+#
+# print(result)
