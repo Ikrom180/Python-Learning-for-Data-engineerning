fbd3564f813ea921088c1fd047f43f713121cb0d
ikrom724 <ikrompersonal@gmail.com>
2026-07-14 18:06:24 +0500

    if Name learned

---
+++ 41IfName/41.If_name_.py
@@ -0,0 +1,21 @@
+# if __name__ = __main__: (this script can be imported or run standalone)
+#                           Functions and classes in this module can be reused
+#                           Without the main block of code executing
+
+# Good Practise (code is modular,
+#                helps readability,
+#                leaves noo global variables,
+#                avoid unitended execution
+
+#  ex. library = Import library for functionality
+#                When tunning library directly, display a help page
+
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
