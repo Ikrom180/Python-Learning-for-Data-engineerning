aa506b9da18f21fc65d0e2edd7dfa080e5d20fee
ikrom724 <ikrompersonal@gmail.com>
2026-07-15 17:56:19 +0500

    Slot machine beginning

---
+++ 43.Python_slot_machine.py
@@ -0,0 +1,22 @@
+#Python Slot Machine
+
+
+def spin_row():
+    pass
+
+def print_row():
+    pass
+
+def get_payout():
+    pass
+
+def main():
+    balance = 100
+
+    print("***************************")
+    print("Welcome to the SlotMachine")
+    print("Symbols: @, #, %, &, $")
+    print("***************************")
+
+if __name__ == '__main__':
+    main()
