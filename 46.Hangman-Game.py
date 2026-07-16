1b3bacbb22c1daafcd31ef7fc0a9350b50ac3075
ikrom724 <ikrompersonal@gmail.com>
2026-07-16 20:20:52 +0500

    Hangman Started

---
+++ 46.Hangman-Game.py
@@ -0,0 +1,72 @@
+#Hangman in Python
+import random
+
+word = ("apple", "orange", "banana", "coconut", "pineapple")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#MY version
+# word = ("apple", "orange", "banana", "coconut", "pineapple")
+#
+# choice = random.choice(word)
+# length = len(choice)
+# middle_name = length // 2
+#
+# result = ""
+# # print(choice)
+#
+#
+#
+# for i in range(length):
+#     if i == middle_name:
+#         result += choice[middle_name]
+#     else:
+#         result += "_"
+#
+# print(result)
