dc019139ca69e76b2dbf7f1f372d43ec8a40c0ea
ikrom724 <ikrompersonal@gmail.com>
2026-07-17 16:23:49 +0500

    Analysis

--- 46.Hangman-Game.py
+++ 46.Hangman-Game.py
@@ -1,8 +1,36 @@
 #Hangman in Python
 import random
+# word = ("apple", "orange", "banana", "coconut", "pineapple")
 
 word = ("apple", "orange", "banana", "coconut", "pineapple")
 
+answer = random.choice(word)
+
+
+def display_hint(hint):
+    print("Hint:", " ".join(hint))
+
+hint = ["_"] * len(answer)
+
+
+# MY version
+is_running = True
+
+while is_running:
+
+
+
+
+    print(answer)
+
+    display_hint(hint)  # You need to define this function
+
+    guess = input("Enter your guess: ").lower()
+
+    if guess in answer:
+        for i in range(len(answer)):
+            if answer[i] == guess:
+                hint[i] = guess
 
 
 
@@ -50,8 +78,8 @@ word = ("apple", "orange", "banana", "coconut", "pineapple")
 
 
 
-
-#MY version
+#
+# # MY version
 # word = ("apple", "orange", "banana", "coconut", "pineapple")
 #
 # choice = random.choice(word)
@@ -70,3 +98,50 @@ word = ("apple", "orange", "banana", "coconut", "pineapple")
 #         result += "_"
 #
 # print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
