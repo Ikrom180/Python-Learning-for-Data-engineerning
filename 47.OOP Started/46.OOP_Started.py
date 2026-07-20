72ce54183ec6cbc3af159612f3c8b4c16e3b1afc
ikrom724 <ikrompersonal@gmail.com>
2026-07-20 17:32:20 +0500

    Class variables

---
+++ 47.OOP Started/46.OOP_Started.py
@@ -0,0 +1,29 @@
+#object = A "bundle" of related attributes (variables) and methods (functions)
+#         Ex. phone, cup, book
+#         You need a "class" to create many objects
+
+# class = (blueprint) used to design the structure and layout of an object
+
+from car import Car
+
+
+car1 = Car("Mustang", 2024, "red", False)
+car2 = Car("Corvette", 2025, "blue", False)
+car3 = Car("Charger", 2026, "yellow", False)
+
+# print(car3.model)
+# print(car3.year)
+# print(car3.color)
+# print(car3.for_sale)
+
+car1.drive()
+car2.drive()
+car3.drive()
+
+car1.stop()
+car2.stop()
+car3.stop()
+
+car1.describe()
+car2.describe()
+car3.describe()
