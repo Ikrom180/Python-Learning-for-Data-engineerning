79eabf9f864906381ed7f004e0031d8962c69da8
ikrom724 <ikrompersonal@gmail.com>
2026-07-20 17:57:02 +0500

    Class variables

---
+++ 48 Class Variables/48.class_Variables.py
@@ -0,0 +1,5 @@
+#Class variables =  shared among all instancwa of a class ## instance variable inside constructor variable ##
+#                   Defined outside the constructors
+#                   Allow you share data among all objects created from that class
+
+
