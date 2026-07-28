d9b2fe497dec42f761200b6ddfe44d5599a46a20
ikrom724 <ikrompersonal@gmail.com>
2026-07-28 11:02:37 +0500

    Property Started

--- 56 Property/56.property.py
+++ 56 Property/56.property.py
@@ -3,8 +3,47 @@
 #             Gives you getter , setter, and deleter method
 
 
+# if we make before variable '_' -> protected if we do like it '__' -> private and you cannot call outside of the class
+# protected variable can be use in child class but private you cannot use it
+# if we do not write without @property it will return method memory addr
+
 class Rectangle:
 
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
+        self._width = width
+        self._height = height
+
+    @property # -> Getter -> read
+    def width(self):
+        return f'{self._width:.1f}cm'
+    @property # -> Getter -> read
+    def height(self):
+        return f'{self._height:.1f}cm'
+
+    @width.setter # Setter -> write
+    def width(self, new_width):
+        if new_width > 0 :
+            self._width = new_width
+        else:
+            print('width must be greater than 0')
+
+    @height.setter # Setter -> write
+    def height(self, new_height):
+        if new_height > 0 :
+            self._height = new_height
+        else:
+            print('height must be greater than 0')
+
+
+
+
+rectangle = Rectangle(3, 4)
+
+# print(rectangle._width) -> this will be 3
+# print(rectangle._height) -> this will be 3
+
+rectangle.width = 5
+rectangle.height = 6
+
+print(rectangle.width)
+print(rectangle.height)
