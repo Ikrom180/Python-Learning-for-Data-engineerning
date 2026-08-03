80df031dfea977eaa6d772b34f3770788e5b490f
ikrom724 <ikrompersonal@gmail.com>
2026-08-03 17:36:53 +0500

    Ending

--- 64 Multithreading/ThreadAnotherResourseFromTim.py
+++ 64 Multithreading/ThreadAnotherResourseFromTim.py
@@ -1,3 +1,26 @@
 import threading
 import time
 
+ls = []
+
+def count(n):
+    for i in range(1, n+1):
+        ls.append(i)
+        time.sleep(0.5)
+
+def count2(n):
+    for i in range(1, n+1):
+        ls.append(i)
+        time.sleep(0.5)
+
+x = threading.Thread(target=count, args=(5,))
+x.start()
+x.join()
+
+y = threading.Thread(target=count2, args=(5,))
+y.start()
+y.join()
+
+
+time.sleep(0.1)
+print(ls)
